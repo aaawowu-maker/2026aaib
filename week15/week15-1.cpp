8f9bf7c67b81905ed09afe2f8f4a5b05d5065ffd
aaawowu-maker <aaawowu@gmail.com>
2026-06-03 11:44:25 +0800

    week15

---
+++ week15/week15-1.cpp
@@ -0,0 +1,16 @@
+// week15-1.cpp SOIT106_ADVANCE_006
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int ans = 0;
+    for (int i = 1; i < N; i++) {
+        ans += i * (i + 1);
+    }
+
+    printf("%d\n", ans);
+
+    return 0;
+}
